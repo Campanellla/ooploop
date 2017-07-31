5030d1a17e02a58cec7e4c585b0c4dcfa7b13518
Campanellla <Campanellla@users.noreply.github.com>
2017-07-31 14:59:26 +0300

    Add files via upload

---
+++ ooploop.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var util = require('util');
+var url = require('url');
+
+var querystring = require('querystring');
+var static = require('node-static');
+var file = new static.Server('.');
+
+var exprss = require('express');
+var app = new exprss();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+function grab(flag){
+	var index = process.argv.indexOf(flag);
+	if (index === -1) {return null;} else {return process.argv[index+1]};
+}
+
+var directory = grab('--dir');
+var start_directory = __dirname;
+var node_program_directory = process.argv[0];
+
+/*
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+*/
+app.use(exprss.static(__dirname+'/'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('nick: '+ msg.nickname+' message: ' + msg.message);
+    io.emit('chat message', msg);
+  });
+});
+
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
+
